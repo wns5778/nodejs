a0bd68af943f655a1bdb6c7d19d92ce35f3a60f0
조영준 <wns5778@naver.com>
2021-10-10 18:03:44 +0900

    nodejs공부 2일

---
+++ sysntax/Template.js
@@ -0,0 +1,7 @@
+var name = 'youngjun'
+var letter = name + '\nadbngja nb one aonvad '+name+' fovnj k '+name
+console.log(letter);
+
+var letter = `${name}
+adbngja nb one aonvad ${name} fovnj k ${name}`;
+console.log(letter);
