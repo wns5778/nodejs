6daccb2042ee0b65e1e1df7094b47fbd93e878ce
조영준 <wns5778@naver.com>
2021-10-11 20:34:48 +0900

    nodejs공부 3일

--- main.js
+++ main.js
@@ -5,38 +5,97 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    console.log(queryData.id);
-    if(_url == '/'){
-      title = "Welcome";
+    var pathname = url.parse(_url, true).pathname;
+
+    if(pathname ==='/'){
+      if(queryData.id === undefined){
+
+        fs.readdir('./data', function(error, filelist){
+          var title = "Welcome";
+          var description = "Hello, Node.js";
+
+          /*
+          var list = `<ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>`;
+          */
+
+          var list = '<ul>';
+
+          var i = 0;
+          while(i < filelist.length){
+            list = list + `<li><a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i+1;
+          }
+
+          list = list+'<ul>';
+
+
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        })
+
+      }
+      else{
+        fs.readdir('./data', function(error, filelist){
+          var title = "Welcome";
+          var description = "Hello, Node.js";
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist.length){
+            list = list + `<li><a href = "/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i+1;
+          }
+          list = list+'<ul>';
+
+        fs.readFile(`data/${queryData.id}`,'utf8',
+        function(err,description){
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+      }
+
     }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
+    else{//올바르지 않은 사이트로 접속한 경우
+      response.writeHead(404);
+      response.end("Not found");
     }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`,'utf8',
-    function(err,description){
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
-      response.end(template);
-    })
+
+
 
 
 
---
+++ syntax/array.js
@@ -0,0 +1,6 @@
+var arr = ['A', 'B', 3, 'D'];
+console.log(arr[2]);
+arr[2] = 'C';
+console.log(arr);
+arr.push('E');
+console.log(arr);
