2bcfdfdce1098ade85cb6050b1bd3260d1038721
조영준 <wns5778@naver.com>
2021-10-19 22:13:28 +0900

    nodejs공부 7일

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
   return `
   <!doctype html>
   <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href = "/create">create</a>
+    ${control}
     ${body}
   </body>
   </html>
@@ -41,11 +41,10 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data', function(error, filelist){
           var title = "Welcome";
           var description = "Hello, Node.js";
-
           var list = templateList(filelist);
-
-
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          var template = templateHTML(title, list,
+             `<h2>${title}</h2>${description}`,
+             `<a href = "/create">create</a>`);
           response.writeHead(200);
           response.end(template);
         })
@@ -58,7 +57,9 @@ var app = http.createServer(function(request,response){
         function(err,description){
           var title = queryData.id;
           var list = templateList(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          var template = templateHTML(title, list,
+             `<h2>${title}</h2>${description}`,
+           `<a href = "/create">create</a><a href = "/update?id=${title}">update</a>`);
           response.writeHead(200);
           response.end(template);
         });
@@ -78,7 +79,7 @@ var app = http.createServer(function(request,response){
             <p><textarea name = "description" placeholder = "descripton"></textarea></p>
             <p><input type="submit"></p>
             </form>
-`);
+`,'');
           response.writeHead(200);
           response.end(template);
         })
