f5eefdd901c70e5f10f997864610a8d4f0f3a032
조영준 <wns5778@naver.com>
2021-10-23 22:14:20 +0900

    nodejs공부 10일

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 
 
 
@@ -35,13 +36,17 @@ var app = http.createServer(function(request,response){
         fs.readFile(`data/${filteredId}`,'utf8',
         function(err,description){
           var title = queryData.id;
+          var sanitizeTitle = sanitizeHtml(title);
+          var sanitizeDescription = sanitizeHtml(description, {
+            allowedTags:['h1']
+          });
           var list = template.List(filelist);
-          var html = template.Html(title, list,
-             `<h2>${title}</h2>${description}`,
+          var html = template.Html(sanitizeTitle, list,
+             `<h2>${sanitizeTitle}</h2>${sanitizeDescription}`,
            `<a href = "/create">create</a>
-            <a href = "/update?id=${title}">update</a>
+            <a href = "/update?id=${sanitizeTitle}">update</a>
             <form action="process_delete" method="post">
-              <input type="hidden" name="id" value="${title}">
+              <input type="hidden" name="id" value="${sanitizeTitle}">
               <input type="submit" value="delete">
             </form>`
           );
